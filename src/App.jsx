f5181e8a52e56bfad6ab1df80421fee352119e92
Rocky <stmjaywardhan912297@gmail.com>
2024-03-20 12:09:35 +0530

    portfilio commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ function App() {
          <div className="relative z-0 bg-primary">
           <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
              <Navbar></Navbar>
-            {/* <Hero></Hero>  */}
+            <Hero></Hero> 
           </div>
-          {/* <About /> */}
-          {/* <Experience />
-          <Tech /> */}
-          <Works />
-           <Feedbacks />
+          <About />
+          <Experience />
+          <Tech />
+          {/* <Works />
+           <Feedbacks /> */}
 
           <div className="relative z-0">
             <Contact />
